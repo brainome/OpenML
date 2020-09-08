bc94f3e89276b5ca48b0db9093192cf679d952aa
zstone-brainome <67713239+zstone-brainome@users.noreply.github.com>
2020-09-08 13:14:17 -0700

    Update _validate.py

--- _validate.py
+++ _validate.py
@@ -1,20 +1,7 @@
 import os
 import pickle
 
-d=pickle.load(open("all_predictors.pickle", "rb"))
-predictors=[("Predictors/"+pred, pred.replace("binary.", "").replace("multi.", "").replace(".py", ".csv"), int(d["ncorrect"][pred.replace(".py", "")])) for pred in [x for x in next(os.walk("./Predictors/"))[2]]]
+predictors=[("Predictors/"+pred, pred.replace("binary.", "").replace("multi.", "").replace(".py", ".csv")) for pred in [x for x in next(os.walk("./Predictors/"))[2]]]
 
 for pred,data,ncorrect in predictors:
-	os.system(f"python3 {pred} Data/{data} -validate > dummy.txt")
-
-
-	with open("dummy.txt") as f:
-		text=f.read()
-	start=text.find("Model accuracy:")
-	subtext=text[start:]
-	end=subtext.find("correct")+len("correct")
-	subtext=subtext[:end].strip()
-	res=int(subtext.split()[-2].replace("(", "").split("/")[0])
-	if res!=ncorrect:
-		print(data)
-		raise Exception
+	os.system(f"python3 {pred} Data/{data} -validate")
