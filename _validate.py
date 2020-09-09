153a32b5defe41f20c025f1c23b7a1e276b2afbd
zachary <zachary.stone@brainome.ai>
2020-09-09 12:02:25 -0700

    update validate

--- _validate.py
+++ _validate.py
@@ -5,9 +5,9 @@ predictors=[pred for pred in next(os.walk("./Predictors/"))[2]]
 for pred in predictors:
 	print("-"*50)
 	if pred[:len("binary.")]=="binary.":
-		data=pred.replace("binary.", "").replace(".py", ".csv")
+		data=pred[len("binary."):].replace(".py", ".csv")
 	elif pred[:len("multi.")]=="multi.":
-		data=pred.replace("multi.", "").replace(".py", ".csv")
+		data=pred[len("multi."):].replace(".py", ".csv")
 	else:
 		data=pred.replace(".py", ".csv")
 	res=os.system(f"python3 Predictors/{pred} Data/{data} -validate")
