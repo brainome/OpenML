120889ea04fb6e4f3f73414c216f278f8a30ddf8
zstone-brainome <67713239+zstone-brainome@users.noreply.github.com>
2020-09-08 17:44:55 -0700

    Update _validate.py

--- _validate.py
+++ _validate.py
@@ -1,7 +1,7 @@
 import os
 
-predictors=[("Predictors/"+pred, pred.replace("binary.", "").replace("multi.", "").replace(".py", ".csv")) for pred in [x for x in next(os.walk("./Predictors/"))[2]]]
+predictors=[(pred, pred.replace("binary.", "").replace("multi.", "").replace(".py", ".csv")) for pred in [x for x in next(os.walk("./Predictors/"))[2]]]
 
 for pred,data in predictors:
 	print("-"*50)
-	os.system(f"python3 {pred} Data/{data} -validate")
+	os.system(f"python3 Predictors/{pred} Data/{data} -validate")
